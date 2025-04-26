9540f97e077b6c574834e2300799eb1cfe661aa2
t0g3pii aka Nico <info@t0g3pii.de>
2025-04-26 08:51:15 +0000

    Edit curl-handler.c

--- source/curl-handler.c
+++ source/curl-handler.c
@@ -242,11 +242,14 @@ void curl_multi_loop_request_setup(int i) {
 	
 	FriendKey friend_key;
 	Result res = FRD_GetMyFriendKey(&friend_key);
+	char header_fc[100];
 	char header_friend_key[100];
 	if (R_SUCCEEDED(res)) {
 		u64 fc;
 		res = FRD_PrincipalIdToFriendCode(friend_key.principalId, &fc);
 		if (R_SUCCEEDED(res)) {
+			snprintf(header_fc, 100, "3ds-fc: %016llX", fc);
+			headers = curl_slist_append(headers, header_fc);
 			u64 fc_seed;
 			res = CFGI_GetLocalFriendCodeSeed(&fc_seed);
 			if (R_SUCCEEDED(res)) {
@@ -281,9 +284,11 @@ void curl_multi_loop_request_setup(int i) {
 	curl_easy_setopt(h->handle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
 	curl_easy_setopt(h->handle, CURLOPT_HTTPHEADER, headers);
 	curl_easy_setopt(h->handle, CURLOPT_CUSTOMREQUEST, h->method);
-	curl_easy_setopt(h->handle, CURLOPT_TIMEOUT, 120);
+	curl_easy_setopt(h->handle, CURLOPT_TIMEOUT, 300); // upper the limit to 5 minutes
 	curl_easy_setopt(h->handle, CURLOPT_SERVER_RESPONSE_TIMEOUT, 10);
 	curl_easy_setopt(h->handle, CURLOPT_CONNECTTIMEOUT, 20);
+	curl_easy_setopt(h->handle, CURLOPT_LOW_SPEED_LIMIT, 10); // min. 10 Bytes/s or slower
+	curl_easy_setopt(h->handle, CURLOPT_LOW_SPEED_TIME, 30);  // ...for 30 seconds .. then abort
 	curl_easy_setopt(h->handle, CURLOPT_NOSIGNAL, 0);
 	curl_easy_setopt(h->handle, CURLOPT_SSL_VERIFYPEER, 1);
 	curl_easy_setopt(h->handle, CURLOPT_CAINFO, "romfs:/certs.pem");
